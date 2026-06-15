f2eaacce453a134af477a547a9fceb620c3ad2b2
Nathan McCormick <mccormicknathan28@gmail.com>
2026-06-15 14:49:57 +1200

    Register exercise API routes protected by auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ExerciseController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function (): void {
+    Route::get('exercises', [ExerciseController::class, 'index']);
+    Route::get('exercises/{exercise}', [ExerciseController::class, 'show']);
+    Route::post('exercises', [ExerciseController::class, 'store']);
+    Route::put('exercises/{exercise}', [ExerciseController::class, 'update']);
+    Route::delete('exercises/{exercise}', [ExerciseController::class, 'destroy']);
+});
